92abd5b2becbef3b2f0267136eaea5b8ecab1bbf
Jonathan Lee <jonlee50@hotmail.com>
2017-01-21 17:12:24 -0500

    added path join

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ let express = require('express');
 //create a instance of the express server
 let app = express();
 
+var path = require('path');
+
 const port = 3000;
 
 app.listen(port);
 console.log('server listening at http://localhost/$(port)');
 
 //app.use(express.static('COMP308-Lab1'))
-app.use('/', express.static('COMP308-Lab1'))
+//app.use('/', express.static('COMP308-Lab1'))
+app.use(express.static(path.join(__dirname, 'root.html')));
 
 //routing
 

6e23a2246dd7e480b4a8e1f79b275af379f97853
Jonathan Lee <jonlee50@hotmail.com>
2017-01-22 00:16:57 -0500

    last try sendfile

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.get('/about', (req, res, next) => {
 app.get('/', (req, res, next) => {
     res.setHeader('Context-Type', 'text/plain');
     //res.send("welcome to express root page!");
-
+    res.sendfile(__dirname + '/root.html');
     next();
 });
